e2fd5331774f537b513291c98cf5781f7c1576d4
ali911-ui <beastali00@gmail.com>
2026-04-17 01:50:32 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
@@ -10,7 +9,3 @@ def home():
 @app.route("/predict", methods=["POST"])
 def predict():
     return jsonify({"status": "working"})
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
